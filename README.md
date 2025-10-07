b4109c9626f689dbf8cb56ef141b6d82c66a6998
Sebastian Hegardt <sehe@netcompany.com>
2025-10-07 14:06:59 +0200

    Improved stack trace parsing and updated readme

--- README.md
+++ README.md
@@ -4,15 +4,29 @@
 
 1. Install Poetry dependency manager: https://python-poetry.org/docs/#installation
 2. From the repository root, run `poetry install`
-   3. This creates a new poetry environment and installs all required dependencies
+   1. This creates a new poetry environment and installs all required dependencies
 
 ## Running
 
-For development it is recommended to use `PyCharm` to run the project from the `main.py` file.
+For development, it is recommended to use `PyCharm` to run the project from the `main.py` file.
 
 The project can also be run from the terminal using `poetry run python .\main.py`
+
+### The simplest way
+
+The easiest way, however, is to just create a little PowerShell script, like 'par2logreader.ps1' and place it in a folder that is on the `PATH`.
+The script can be implemented like:
+
+```ps1
+Push-Location "<dir>\PAR2LogReader"
+poetry run python main.py
+```
+Change `<dir>` to where you've cloned the repo. The log reader can then be launched from any terminal by running the command `par2logreader`.
 
 ## Building executable
 
 To build a single file executable, run `.\build.bat`. This creates an executable file in the `/dist` folder and copies assets.
 
+## Configuration
+
+...
--- src/gui/inspector/stacktrace/StackTraceTable.py
+++ src/gui/inspector/stacktrace/StackTraceTable.py
@@ -7,9 +7,9 @@ from PyQt6.QtWidgets import QHeaderView
 
 from src.gui.abstr.Table import Table, TableModel
 
-REG_AT = re.compile(r'\n+\s+at')
-REG_LINE = re.compile(r':line (\d+)')
-REG_CLASS_METHOD = re.compile(r'^DPA.PAR2.[\w+\.]+\.(\w+)\.(\w+(?:\[\w\])?\([\w\s,]+\))', re.DOTALL)
+REG_STACKTRACE_SPLIT = re.compile(r"at\s(.*)\sin(.*):line\s(\d+)")
+REG_FILE = re.compile(r".*\\(.*)")
+REG_METHOD = re.compile(r".*\.(\w+)\(.*")
 MONO_FONT = QFont('Consolas')
 
 
@@ -29,16 +29,17 @@ class StackTraceTableModel(TableModel):
 
 def _parse_stack_trace(stack_trace: str) -> typing.List[typing.List[str]]:
     out = []
-    at_split = [l.strip() for l in REG_AT.split(stack_trace)]
+    file_locations = REG_STACKTRACE_SPLIT.findall(stack_trace)
 
-    for row in at_split:
-        cm_match = REG_CLASS_METHOD.match(row)
-        line_match = REG_LINE.search(row)
-        if cm_match and line_match:
-            clazz = cm_match.group(1)
-            method = cm_match.group(2)
-            line = line_match.group(1)
-            out.append([clazz, method, line])
+    for full_method, file, line in file_locations:
+        filename_match = REG_FILE.match(file)
+        method_match = REG_METHOD.match(full_method)
+
+        if filename_match and method_match:
+            filename = filename_match.group(1)
+            method = method_match.group(1)
+            line = int(line)
+            out.append([filename, method, line])
 
     return out
 
@@ -51,12 +52,12 @@ class StackTraceTable(Table):
         self._max_col_width = 400
         self.setModel(StackTraceTableModel(self._columns))
 
-        self.horizontalHeader().setSectionResizeMode(0, QHeaderView.ResizeMode.Fixed)
-        self.setColumnWidth(0, 100)
-
-        self.horizontalHeader().setSectionResizeMode(1, QHeaderView.ResizeMode.Stretch)
-        self.horizontalHeader().setSectionResizeMode(2, QHeaderView.ResizeMode.Stretch)
-        self.horizontalHeader().setSectionResizeMode(3, QHeaderView.ResizeMode.ResizeToContents)
+        #self.horizontalHeader().setSectionResizeMode(0, QHeaderView.ResizeMode.Fixed)
+        self.horizontalHeader().setSectionResizeMode(1, QHeaderView.ResizeMode.ResizeToContents)
+        self.horizontalHeader().setSectionResizeMode(2, QHeaderView.ResizeMode.ResizeToContents)
+        self.horizontalHeader().setSectionResizeMode(3, QHeaderView.ResizeMode.Fixed)
+        self.setColumnWidth(0, 80)
+        self.setColumnWidth(3, 80)
         self.verticalHeader().setVisible(False)
 
         self.sortByColumn(0, Qt.SortOrder.AscendingOrder)
@@ -66,4 +67,3 @@ class StackTraceTable(Table):
         self.clear_data()
         data = _parse_stack_trace(stack_trace)
         self.model().extend_data([[i, *d] for i, d in enumerate(data)])
-        self.resize_columns_to_content(self._max_col_width)
